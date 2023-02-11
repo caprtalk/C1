ba4d812bf2355a7d4b33ae93e909c12ca931d051
Caspar Brown <casparmoab@gmail.com>
2023-02-11 01:32:37 -0700

    initial profile class

--- profile.py
+++ profile.py
@@ -1,1 +1,9 @@
 # Class to save and load identifiaction profiles based on facial recognition
+
+
+class profile:
+
+    def __init__(self, name, landmarks):
+        self.name = name
+        self.landmarks = landmarks
+
